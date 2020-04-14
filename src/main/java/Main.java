7417a949e6958da397cc04d666bd2124c4384b04
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-04-14 10:19:45 +0530

    shallow prototype

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import builder.Builder;
 import builder.LunchOrder;
+import prototype.Record;
+import prototype.shallow.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -16,8 +21,25 @@ public class Main {
         System.out.println(lunchOrder.getMeat());
     }
 
+    private static void testShallowPrototype(){
+        // this is shallow prototype because I clone object that will duplicate same object
+        String sql ="select * rom movies where title = ?";
+        List<String> parameters = new ArrayList<String>();
+        parameters.add("star wars");
+        Record record = new Record();
+        Statement statement = new Statement(sql,parameters,record);
+        Statement anotherStatement = statement.clone();
+        System.out.println(statement.getSql());
+        System.out.println(statement.getParameters());
+        System.out.println(statement.getRecord());
+        System.out.println(anotherStatement.getSql());
+        System.out.println(anotherStatement.getParameters());
+        System.out.println(anotherStatement.getRecord());
+    }
+
     public static void main (String[] args){
         Builder builder = new Builder();
         System.out.println(builder.stringBuilderTest());
+        testShallowPrototype();
     }
 }
